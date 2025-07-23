6e5e9718279aed705a399821f5afe6d8a0a0ff1f
theDorrian <65199136+theDorrian@users.noreply.github.com>
2025-07-23 19:31:52 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,32 @@ with col1:
 with col2:
   fig2 = px.scatter(df, x = "bill_length_mm", y = "flipper_length_mm", color = "species", title = "Длина клюва vs Длина крыла")
   st.plotly_chart(fig2, use_container_width = True)
+
+X = df.drop(["species"], axis = 1)
+y = df["species"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
+
+encoder = ce.TargetEncoder(cols = ["island", "sex"])
+X_train_encoded = encoder.fit_transform(X_train, y_train)
+X_test_encoded = encoder.transform(X_test)
+
+models = {
+  "Decision Tree": DecisionTreeClassifier(random_state = 42)
+  "KNN" : KNeighborsClassifier()
+}
+
+result = []
+
+for name, model in models.items():
+  model.fit(X_train_encoded, y_train)
+  acc_train = accuracy_score(y_train, model.predict(X_train_encoded))
+  acc_test = accuracy_score(y_test, model.predict(X_test_encoded))
+  results.append({
+      "Model": name,
+      "Train Accuracy": round(acc_train, 2),
+      "Test Accuracy": round(acc_test, 2) 
+  })
+
+st.write("### Сравнение моделей по точности")
+st.table(pd.DataFrame(results))
