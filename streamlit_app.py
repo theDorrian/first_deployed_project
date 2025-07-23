be2e7a82c0f99a8130302407c7c2ebc71e59cffc
theDorrian <65199136+theDorrian@users.noreply.github.com>
2025-07-23 19:36:34 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ with col2:
 X = df.drop(["species"], axis = 1)
 y = df["species"]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
 
 encoder = ce.TargetEncoder(cols = ["island", "sex"])
 X_train_encoded = encoder.fit_transform(X_train, y_train)
